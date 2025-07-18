f61db0c3a2383fe5a8b066649a13dcb1c7b2b9ce
shahwaizalishahwaizali786-commits <shahwaizali.shahwaizali786@gmail.com>
2025-07-18 21:56:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;7d8yQCIY#381NlvTXs9l8gV4h-4nw9PI7tUbAfXAFcX6bbGbCdaI", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
